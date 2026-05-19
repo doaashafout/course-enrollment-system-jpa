16b805a3798c7594a17d557ced0d824b770f251b
doaashafout <shadoaa2003@gmail.com>
2026-05-19 13:58:15 +0300

    Delete database.sql

--- database.sql
+++
@@ -1,7 +0,0 @@
--- Tables are auto-created by JPA (persistence.xml: schema-generation.database.action=create)
--- This script is only for inserting sample data if needed
-
-USE enrollment_system;
-
-INSERT INTO students (name) VALUES ('Ahmed Ali'), ('Sara Mohamed'), ('Omar Hassan');
-INSERT INTO courses (course_name) VALUES ('Mathematics'), ('Physics'), ('Computer Science');
